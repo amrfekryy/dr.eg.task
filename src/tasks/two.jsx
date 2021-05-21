43587d8e49e85e3a86a2da97a968f8c776c203eb
Amr-Fekry-Bosila <amrfekryy@gmail.com>
2021-05-21 13:01:53 +0200

    task 2 initial

--- src/tasks/two.jsx
+++ src/tasks/two.jsx
@@ -1,3 +1,4 @@
+import {useState, useEffect} from 'react'
 import { BackToHome } from "../App";
 
 /*
@@ -5,7 +6,40 @@ import { BackToHome } from "../App";
   eg: `?page=3&results=10`
 */
 
+export const fetchData = ({page, results=5}) => {
+  const url = `https://randomuser.me/api/?seed=dexi-interview&page=${page}&results=${results}`
+  return fetch(url)
+}
+
 const ChallengeTwo = () => {
+
+  const [users, setUsers] = useState([])
+  const [page, setPage] = useState(1)
+  const [isLoading, setIsLoading] = useState(false)
+  
+  const fetchUsers = (page=1) => {
+    
+    setIsLoading(true)
+    
+    fetchData({ page, results:5 })
+    .then(response => response.json())
+    .then(({ results:new_users=[] }) => {
+      // console.log('current users', users)
+      // console.log('new users', new_users);
+      if ( new_users.length ) {
+        // append new users to existing users
+        setUsers([...users, ...new_users])
+        setIsLoading(false)
+      }
+    })
+    .catch(() => setIsLoading(false))
+  }
+
+  useEffect(() => {
+    // get first 5 users on initial render
+    if (!users.length) fetchUsers()
+  }, [])
+
   return (
     <>
       <BackToHome />
@@ -27,7 +61,48 @@ const ChallengeTwo = () => {
         the next page of the API and appends the results to the existing users.
       </h2>
 
+      <br /><h1 className="title is-3 has-text-white">Solution:</h1>
+
       {/* Insert your table code here */}
+      <table 
+        className='table is-hoverable is-fullwidth'
+        style={{borderRadius: '10px'}}
+      >
+        <thead>
+          <tr>
+            {['Title', 'First', 'Last', 'Phone'].map(head => <th key={head}>{head}</th>)}
+          </tr>
+        </thead>
+        <tbody>
+          { !isLoading && !users.length
+            ? <tr><td>No data! Please check your connection and refresh the page.</td></tr> 
+            : users.map((user={}) => {
+              // destructure user data
+              const {
+                name: {title, first, last}, 
+                email, picture,
+                cell, phone, 
+                location, 
+                id, gender, dob, nat, registered,
+                login: {uuid}, 
+              } = user
+
+              return <tr key={uuid}>
+                <td>{title}</td>
+                <td>{first}</td>
+                <td>{last}</td>
+                <td>{phone}</td>
+              </tr>
+            })
+          }
+        </tbody>
+      </table>
+
+      <button onClick={()=>{ fetchUsers(page+1); setPage(page+1) }}
+      >
+        {isLoading? 'Loading...' : 'Load More'}
+      </button>
+
     </>
   );
 };
